ce6c6c8c97f39e09209be1376da801008ac9cfc2
David J. Hammaker <davidjhammaker@gmail.com>
2022-02-27 18:13:41 -0500

    Add minimal Slate editor

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
+import { createEditor } from "slate";
+import { Slate, Editable, withReact } from "slate-react";
 import "./App.css";
-import TextBox from "./components/TextBox";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <TextBox />
-      </div>
-    );
-  }
-}
+const App = () => {
+  const [editor] = useState(() => withReact(createEditor()));
+  const [value, setValue] = useState([
+    {
+      type: "paragraph",
+      children: [{ text: "some text" }],
+    },
+  ]);
+  return (
+    <Slate
+      editor={editor}
+      value={value}
+      onChange={(newValue) => setValue(newValue)}
+    >
+      <Editable />
+    </Slate>
+  );
+};
 
 export default App;
