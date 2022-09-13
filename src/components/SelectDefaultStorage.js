301e6cf864812cc29d5d973b4ce257e718a1cc32
David J. Hammaker <davidjhammaker@gmail.com>
2022-09-12 23:06:52 -0400

    Refine storage selection and send PATCH on update

--- src/components/SelectDefaultStorage.js
+++ src/components/SelectDefaultStorage.js
@@ -1,8 +1,8 @@
 import { useState, useEffect } from "react";
-import FormControl from "@mui/material/FormControl";
-import InputLabel from "@mui/material/InputLabel";
 import Select from "@mui/material/Select";
 import MenuItem from "@mui/material/MenuItem";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 import Cookies from "js-cookie";
 import axios from "axios";
 import { backendOrigin } from "./utils/navTools";
@@ -34,21 +34,64 @@ const SelectDefaultStorage = (props) => {
     }
   }, [storageOptions]);
 
+  const handleSelectStorage = (event) => {
+    axios
+      .patch(
+        `${backendOrigin}/config/`,
+        {
+          default_storage: event.target.value,
+        },
+        {
+          headers: { Authorization: `token ${Cookies.get("token")}` },
+        }
+      )
+      .then(
+        props.setConfig({
+          ...props.config,
+          default_storage: event.target.value,
+        })
+      )
+      .catch((error) => console.log(error));
+  };
+
   return (
-    <FormControl sx={{ minWidth: 160 }}>
-      <InputLabel id="default-storage-label">Default Storage</InputLabel>
-      <Select labelId="default-storage-label" label="Default Storage">
-        {storageOptions &&
-          Object.keys(storageOptions).map((storageOptionKey) => (
-            <MenuItem
-              key={storageOptions[storageOptionKey]}
-              value={storageOptions[storageOptionKey]}
-            >
-              {storageOptionKey}
-            </MenuItem>
-          ))}
-      </Select>
-    </FormControl>
+    <Grid
+      container
+      justifyContent="center"
+      alignItems="center"
+      direction="column"
+    >
+      <Grid item>
+        <Typography fontSize="small">Default Storage</Typography>
+      </Grid>
+      <Grid item>
+        {storageOptions && (
+          <Select
+            value={
+              props.config.default_storage === ""
+                ? "placeholderValue"
+                : props.config.default_storage
+            }
+            onChange={handleSelectStorage}
+          >
+            {props.config.default_storage === "" && (
+              <MenuItem value="placeholderValue">
+                (Select Default Storage)
+              </MenuItem>
+            )}
+            {storageOptions &&
+              Object.keys(storageOptions).map((storageOptionKey) => (
+                <MenuItem
+                  key={storageOptions[storageOptionKey]}
+                  value={storageOptions[storageOptionKey]}
+                >
+                  {storageOptionKey}
+                </MenuItem>
+              ))}
+          </Select>
+        )}
+      </Grid>
+    </Grid>
   );
 };
 
